364236a81e69f2e5432b031541cae920f3318153
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-07-30 21:42:22 +0200

    use env var

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@ Class Config
         'password' => "azerty",
 
         // Storage mode (sql, csv, log)
-        'mode' => 'sql',
+        'mode' => 'log',
 
         // Directory path for log & CSV mode
         'dirpath' => "test",
@@ -15,11 +15,11 @@ Class Config
         // Database parameters
         'db' => array(
             'mode' => 'mysql', // mysql or pgsql
-            'host'=> getenv("MYSQL_ADDON_HOST"),
-            'port' => getenv("MYSQL_ADDON_PORT"),
-            'dbname' => getenv("MYSQL_ADDON_DB"),
-            'username' => getenv("MYSQL_ADDON_USER"),
-            'password' => getenv("MYSQL_ADDON_PASSWORD"),
+            'host'=> "localhost",
+            'port' => "3306",
+            'dbname' => "http-logs-drain",
+            'username' => "root",
+            'password' => "",
             'table' => "logs"
             )
     );
