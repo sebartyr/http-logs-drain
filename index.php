d77b116fd2417815b482fdb37e2d888e89425505
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-07-31 19:10:51 +0200

    fix

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-$post_content = file_get_contents('php://input');
-
-if (!fastcgi_finish_request()) syslog(LOG_ERR, "KO");
-
 require_once('includes/LogsProcessor.class.php');
 require_once('includes/config.php');
 require_once('includes/login.php');
@@ -11,21 +7,22 @@ header("Content-Type: application/json");
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
+    $post_content = file_get_contents('php://input');
     if(!empty($post_content))
     {
-        $lp = new LogsProcessor($post_content, Config::$config['mode']);
+        /*$lp = new LogsProcessor($post_content, Config::$config['mode']);
         if($lp->write())
         {
             $message = 'Logs have been saved';
             syslog(LOG_INFO, $message);
             echo '{"status": "'.$message.'"}';
-        }
-        else
-        {
+        }*/
+        //else
+        //{
             $message = 'An error occured';
             syslog(LOG_ERR, $message);
             echo '{"status": "'.$message.'"}';
-        }
+        //}
     }
     else
     {
