6f9e2bb623a1a872bad10cdd95dc530bf2b8b500
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-08-03 22:08:54 +0200

    test mysql

--- includes/config.php
+++ includes/config.php
@@ -7,9 +7,9 @@ define("MODE", "log");
 define("DIRPATH", "test");
 
 define("DB_MODE", "mysql");
-define("DB_HOST", "localhost");
-define("DB_PORT", "3306");
-define("DB_NAME", "http-logs-drain");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_HOST", getenv("MYSQL_ADDON_HOST"));
+define("DB_PORT", getenv("MYSQL_ADDON_PORT"));
+define("DB_NAME", getenv("MYSQL_ADDON_DB"));
+define("DB_USERNAME", getenv("MYSQL_ADDON_USER"));
+define("DB_PASSWORD", getenv("MYSQL_ADDON_PASSWORD"));
 define("DB_TABLE", "logs");
