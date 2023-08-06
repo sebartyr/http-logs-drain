98739f50a1ce5ca0a30db104b0f42d060e4fea65
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-08-07 00:08:43 +0200

    work in progress - add new features : fix some issues#2

--- convertlogs/index.php
+++ convertlogs/index.php
@@ -22,7 +22,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET')
     {
         $message = 'Logs have been converted';
         syslog(LOG_INFO, $message);
-        //echo '{"state": "'.$message.'", "link": "'.$lc->convert().'"}';
+        echo '{"state": "'.$message.'", "link": "'.$lc->convert().'"}';
     }
     else
     {
