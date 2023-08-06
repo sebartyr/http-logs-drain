0e7b74323481feb39b7874cdff138fdb41ddfa86
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-08-07 00:14:05 +0200

    work in progress - add new features : fix some issues#3

--- convertlogs/index.php
+++ convertlogs/index.php
@@ -5,7 +5,7 @@ require_once('../includes/Tools.class.php');
 require_once('../includes/config.php');
 require_once('../includes/login.php');
 
-//header("Content-Type: application/json");
+header("Content-Type: application/json");
 
 if($_SERVER['REQUEST_METHOD'] == 'GET')
 {
