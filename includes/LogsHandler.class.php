e32ba5fad167db352511fe90764117e2bc6ba875
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-09-20 23:08:30 +0200

    fix path for converting

--- includes/LogsHandler.class.php
+++ includes/LogsHandler.class.php
@@ -56,7 +56,7 @@ class LogsHandler
                 {
                     $proto = (!empty($_SERVER['https']))?"https":"http";
                     $port = ($_SERVER['SERVER_PORT'] != 80 && $_SERVER['SERVER_PORT'] != 443)?':'.$_SERVER['SERVER_PORT']:"";
-                    return $proto.'://'.$_SERVER['SERVER_NAME'].$port.'/convertlogs/'.$lp->getDirpath().'/'.$lp->getFullFilename();
+                    return $proto.'://'.$_SERVER['SERVER_NAME'].$port.'/convert/'.$lp->getDirpath().'/'.$lp->getFullFilename();
                 }
 
             }
